24e53565eaec5ba5ba81832707ea6f9dc1e0298c
Andrew Chang <21andrewch@gmail.com>
2025-11-28 14:56:04 -0800

    feat: icons

--- apps/web/src/lib/components/md/PropertyList.svelte
+++ apps/web/src/lib/components/md/PropertyList.svelte
@@ -129,16 +129,6 @@
 								onclick={() => onSelectUnit(property, unit)}
 							>
 								<div class="flex flex-row gap-2">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="currentColor"
-										class="bi bi-house-door-fill h-4 w-4"
-										viewBox="0 0 16 16"
-									>
-										<path
-											d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"
-										/>
-									</svg>
 									{unit.label}
 								</div>
 								<div class="flex flex-row gap-2">
